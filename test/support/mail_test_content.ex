f5553eba5e5dc5f8f5d934d36e16ca7205bc6b93
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-02-24 11:25:40 +0100

    do not use template path directly

--- test/support/mail_test_content.ex
+++ test/support/mail_test_content.ex
@@ -1,8 +1,9 @@
 defmodule Doorman.MailTestContent do
   require EEx
 
-  EEx.function_from_file(:def, :welcome_subject, "priv/templates/welcome.subject", [:locale, :user, :meta])
-  EEx.function_from_file(:def, :welcome_html_body, "priv/templates/welcome.html", [:locale, :user, :meta])
-  EEx.function_from_file(:def, :welcome_text_body, "priv/templates/welcome.text", [:locale, :user, :meta])
+
+  EEx.function_from_file(:def, :welcome_subject,  Path.join(["#{:code.priv_dir(:doorman)}", "templates", "welcome.subject"]), [:locale, :user, :meta])
+  EEx.function_from_file(:def, :welcome_html_body, Path.join(["#{:code.priv_dir(:doorman)}", "templates", "welcome.html"]), [:locale, :user, :meta])
+  EEx.function_from_file(:def, :welcome_text_body, Path.join(["#{:code.priv_dir(:doorman)}", "templates", "welcome.text"]), [:locale, :user, :meta])
   
 end
