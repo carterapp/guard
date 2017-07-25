5d21977d168d0e0b151055c7f8c2708cda77dcf8
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-07-25 03:26:07 +0200

    add helper function for sending notifications

--- lib/doorman/pusher.ex
+++ lib/doorman/pusher.ex
@@ -5,6 +5,10 @@ defmodule Doorman.Pusher do
     Doorman.Pusher.Server.send_user_message(Doorman.Pusher.Server, user, message)
   end
 
+  def send_user_notification(user, title, body) do
+    send_user_message(user, %{notification: %{body: body, title: title}})
+  end
+
   def send_message(message) do
     Doorman.Pusher.Server.send_message(Doorman.Pusher.Server, message)
   end
