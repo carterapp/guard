a4d95d5629f47b42651394facb1c7637d865fcaf
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2017-11-11 13:13:00 +0100

    log remote ip

--- lib/doorman/plug/audit_logger.ex
+++ lib/doorman/plug/audit_logger.ex
@@ -12,6 +12,11 @@ defmodule Doorman.Plug.AuditLogger do
   end
 
   def call(conn, _level) do
+    remote_ip = case Plug.Conn.get_req_header(conn, "x-forwarded-for") do
+      [forwarded|_rest] -> forwarded
+      _ -> Enum.join(Tuple.to_list(conn.remote_ip), ".")
+    end
+    Logger.metadata(remote_ip: remote_ip)
     case Authenticator.current_claims(conn) do
       { :ok, _claims } ->
         user = Guardian.Plug.current_resource(conn)
