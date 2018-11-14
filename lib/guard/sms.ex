84bffe2fd71e8726a715250f3cb42ec1c901d37b
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2018-11-14 13:45:13 +0100

    send_template_with_options

--- lib/guard/sms.ex
+++ lib/guard/sms.ex
@@ -22,6 +22,14 @@ defmodule Guard.Sms do
     send_message(user, message, callback)
   end
 
+  def send_template_with_options(%User{} = user, type, meta, options=%{}, callback \\ nil) do
+    module = Map.get(email_setup()[:templates], type)
+    locale = user.locale
+    message =  apply(module, :text_body, [locale, user, meta])
+    send_message(user, Map.put(options, :message, message), callback)
+  end
+
+
   #Templates are kept in the mailer module
   defp email_setup do
     Application.get_env(:guard, Guard.Mailer)
