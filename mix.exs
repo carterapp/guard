3389cbefb4f2004d715b294eafc909ba1cf9a361
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2019-05-14 10:49:28 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Guard.Mixfile do
   def project do
     [
       app: :guard,
-      version: "0.14.8",
+      version: "0.14.9",
       elixir: "~> 1.4 or ~> 1.5 or ~> 1.6 or ~> 1.7 or ~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
